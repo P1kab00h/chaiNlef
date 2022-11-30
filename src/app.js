f215ea6615349b58d5f05c5e6f1015b40e07de02
P1kab00h <nicolas.lefaure@gmail.com>
2022-11-30 10:20:25 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -16,6 +16,37 @@ class Cube {
     }
 }
 
-module.exports = {
-    Cube:Cube
+class Triangle {
+    constructor(sideLength1, sideLength2, sideLength3) {
+        this.sideLength1 = sideLength1;
+        this.sideLength2 = sideLength2;
+        this.sideLength3 = sideLength3;
+    }
+
+    getSideLength1() {
+        return this.sideLength1;
+    }
+    getSideLength2() {
+        return this.sideLength2;
+    }
+    getSideLength3() {
+        return this.sideLength3;
+    }
+
+    isTriangle() {
+        if ((this.sideLength1 + this.sideLength2) > this.sideLength3)
+        {
+            return true;
+        }
+    }
 }
+
+module.exports = {
+    Cube:Cube,
+    Triangle:Triangle
+}
+
+let testTriangle = new Triangle(1,2,3);
+let testIsTriangle = testTriangle.isTriangle();
+
+console.log(testIsTriangle);
--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,9 @@
 const Cube = require('../src/app').Cube;
+const Triangle = require('../src/app').Triangle;
+
 const expect = require('chai').expect;
+
+
 
 describe('Testing the Cube Functions', function() {
     it('1. The side length of the Cube', function(done) {
@@ -20,4 +24,21 @@ describe('Testing the Cube Functions', function() {
         done();
     });
     
+});
+
+describe('Testing the Triangle Functions', function() {
+    it('1. Check the 3 sides length', function(done) {
+        let triangle1 = new Triangle(5, 4, 3);
+        expect(triangle1.getSideLength1()).to.equal(5);
+        expect(triangle1.getSideLength2()).to.equal(4);
+        expect(triangle1.getSideLength3()).to.equal(3);
+        done();
+    });
+    it(`2. Check if it's a triangle`, function(done) {
+        let triangle2 = new Triangle(1, 2, 3);
+        expect(triangle2.isTriangle()).to.equal(true);
+        done();
+    });     
+
+
 });
